16ed8067d4ff2530a09b21b2527dfc4c1b4b5070
Fatih Toker <fatihhtoker@gmail.com>
2023-11-04 03:56:48 +0100

    feat: add email address

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import Image from 'next/image'
 
 export default function Home() {
@@ -5,6 +6,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center p-24">
      <h1>Welcome to the jungle.</h1>
     <p>This page is under construction.</p>
+    <Link href="mailto:fatihhtoker@gmailcom">fatihhtoker@gmail.com</Link>
     </main>
   )
 }
